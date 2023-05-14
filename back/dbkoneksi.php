7f59b41df786d0054e11775483739938dd50965e
NurAfifRaihan <114470342+NurAfifRaihan@users.noreply.github.com>
2023-05-14 12:27:45 +0700

    Project Toko Motor

--- back/dbkoneksi.php
+++ back/dbkoneksi.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <?php 
   $host = 'localhost';
-  $db = 'toko_motor';
-  $user = 'root';
-  $pass = '';
+  $db = 'db_nura22053ti';
+  $user = 'nura22053ti';
+  $pass = '16320110222053';
   $charset='utf8mb4';
 
   $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
